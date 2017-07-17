bc229bfe0d054a55f2b5b6844c0445f2e6f27181
dilshan hewavithara <dilshan.15@cse.mrt.ac.lk>
2017-07-18 01:18:57 +0530

    Updated

--- Daily Report final for rental.php
+++ Daily Report final for rental.php
@@ -7,8 +7,6 @@
 		include 'connection.php';
 		$generate=$_POST['generate'];
 		$date=$_POST['select_date'];
-		
-		
 	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
