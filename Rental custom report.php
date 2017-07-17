373d33f267b28f725d7daa800d5b85322bfa40b1
dilshan hewavithara <dilshan.15@cse.mrt.ac.lk>
2017-07-18 01:23:20 +0530

    Updated

--- Rental custom report.php
+++ Rental custom report.php
@@ -7,9 +7,7 @@
 	$bool=false;
 	if (isset($_POST['getcustom'])){	
 		$go=$_POST['getcustom'];
-		$search=$_POST['search'];
-		
-}
+		$search=$_POST['search'];}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
